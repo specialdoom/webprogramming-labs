46dd7f99feea0a385c0f58662367097cffcdb771
specialdoom <bledea.bogdan97@gmail.com>
2024-03-04 13:22:39 +0200

    chore: use data

---
+++ lab1/pug/data/index.js
@@ -0,0 +1,96 @@
+const links = [{
+    href: "#",
+    title: "Battle Royale"
+}]
+
+const modes = [
+    {
+        title: "Battle Royale",
+        description: "Be the last squad standing in a 100-player PvP fight. Scavenge for weapons, build forts, and fight to the finish.",
+        href: "#"
+    },
+    {
+        title: "Creative",
+        description: "Unleash your creativity and build anything you can imagine. Play solo or with friends and explore endless possibilities.",
+        href: "#"
+    },
+    {
+        title: "Save the World",
+        description: "Team up with friends to fight off hordes of monsters and complete cooperative missions to save the world from darkness.",
+        href: "#"
+    }
+]
+
+const tips = [
+    {
+        title: "Master the Building Mechanics:",
+        description: "Learning to build quickly and effectively is crucial for success in Fortnite."
+    },
+    {
+        title: "Utilize the Environment:",
+        description: "Use natural cover and build your own structures to gain an advantage over your opponents."
+    },
+    {
+        title: "Be Aware of Your Surroundings:",
+        description: "Always pay attention to the sounds of gunfire and footsteps to avoid getting surprised."
+    },
+    {
+        title: "Work with Your Team:",
+        description: "Communication and teamwork are key to winning in Fortnite."
+    }
+]
+
+const features = [
+    {
+        title: "Constant Updates:",
+        description: "New weapons, items, and gameplay mechanics are added regularly, keeping the game fresh and exciting."
+    },
+    {
+        title: "Building Mechanics:",
+        description: "Construct forts, ramps, and other structures to gain an advantage in combat or protect yourself from enemies."
+    },
+    {
+        title: "Customization:",
+        description: "Personalize your character with outfits, emotes, and gliders to express yourself on the battlefield."
+    },
+    {
+        title: "Social Play:",
+        description: "Team up with friends or squad up with strangers to battle together and achieve victory."
+    }
+]
+
+const skins = [
+    {
+        name: "Black Knight",
+        rarity: "Legendary",
+        release: "Season 2",
+        image: "https://fortnite.gg/img/items/892/icon.jpg?2"
+    },
+    {
+        name: "Omega",
+        rarity: "Legendary",
+        release: "Season 4",
+        image: "https://fortnite.gg/img/items/811/icon.jpg?3"
+    }
+]
+
+const articles = [
+    {
+        title: "New Season: Chapter 4 - Paradise Found",
+        description: "Dive into a brand new island with fresh locations, exciting challenges, and a new chapter in the Fortnite story.",
+        link: {
+            href: "#",
+            label: "Read more"
+        }
+    },
+    {
+        title: "Weapon Balance Update",
+        description: "Several weapons have been adjusted to ensure a more balanced and enjoyable gameplay experience.",
+        link: {
+            href: "#",
+            label: "View Patch Notes"
+        }
+    }
+]
+
+module.exports = { links, tips, features, modes, skins, articles };
--- lab1/pug/index.js
+++ lab1/pug/index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-const links = [{
-  href: "#",
-  title: "Battle Royale"
-}]
+const { links, modes, tips, features, skins, articles } = require("./data")
 
 app.set("view engine", "pug")
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Fortnite', links: links })
+  res.render('index', { title: 'Fortnite', links, modes, tips, features, skins, articles })
 })
 
 app.listen(port, () => {
